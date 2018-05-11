64941e4a19678100d1f75fa372f7ca5271e3e772
xxyxiaoya <xxyxiaoya@163.com>
2018-05-11 14:24:36 +0800

    update

--- loginCode_data.py
+++ loginCode_data.py
@@ -1,5 +1,5 @@
 # -*- coding: utf8 -*-
 
-loginCode = ['dhzc124','xsjl124','xdjmsh124','qydd124','xdyy124','fwzczj124','kfgl124','xdjq124','tqjq124','cwzx124','zlfzzc124']
-
+#loginCode = ['dhzc124','xsjl124','xdjmsh124','qydd124','xdyy124','fwzczj124','kfgl124','xdjq124','tqjq124','cwzx124','zlfzzc124']
+loginCode = ['xiaolu','xiaolu','xiaolu','xiaolu','xiaolu','xiaolu','xiaolu','xiaolu','xiaolu','xiaolu','xiaolu']
 
--- 新建用户/loginCode_data.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf8 -*-
-
-loginCode = ['dhzc124','xsjl124','xdjmsh124','qydd124','xdyy124','yygl124','kfgl124','xdjq124','tqjq124','cwzx124','zlfzzc124']
